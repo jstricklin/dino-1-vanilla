6512a0cff429f39695359af76a857c9ddbf8a5c6
Jose Stricklin <josestricklin@El-Diablo.local>
2018-09-04 15:23:59 -0600

    add vanilla components

--- components/JobListing.js
+++ components/JobListing.js
@@ -1,22 +1,18 @@
-class Listing {
+export class Listing {
     constructor(title, pay, description) {
          this.title = title
          this.pay = pay
          this.description = description
+        this.interested = []
     }
-
-
-
+    render(){
+        let li = document.createElement('li')
+    li.innerHTML = `
+        <h3>${this.title}</h3>
+        <small>${this.pay}</small>
+        <p>${this.description}</p>
+        <small>${this.interested.length} dinosaurs are interested in this job
+        `
+        return li
+    }
 }
-
-
-module.exports = {
-    Listing
-}
-// function test(){
-//     console.log("test")
-// }
-
-// modules.export = {
-//     test
-// }
